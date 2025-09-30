e53772256463437e8339e158cf573b089338d6f8
101501186 <101501186@georgebrown.ca>
2025-09-30 12:03:26 -0500

    part6 - Get the UI references

--- app/src/main/java/com/example/a1_tyson_ward_dicks/MainActivity.java
+++ app/src/main/java/com/example/a1_tyson_ward_dicks/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.a1_tyson_ward_dicks;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText workHoursInput, hourlyRateInput;
+    Button btnSubmit, btnViewList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Get the UI references
+        workHoursInput = findViewById(R.id.work_Hours);
+        hourlyRateInput = findViewById(R.id.hours_Rate);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnViewList = findViewById(R.id.btnView_List);
+        
+
     }
+
+
+
 }
